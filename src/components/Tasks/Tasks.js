1883abd8e3931e27c6f5bb084314b87a4ce68117
Saikat Chakraborty <saikatgpae@gmail.com>
2024-07-25 10:47:09 +0530

    Remove linters error.

--- src/components/Tasks/Tasks.js
+++ src/components/Tasks/Tasks.js
@@ -1,34 +1,27 @@
-/* eslint-disable */
+// /* eslint-disable */
 import { useDispatch, useSelector } from 'react-redux';
 import './Tasks.css';
+import { useEffect } from 'react';
 import AddTasks from '../AddTasks/AddTasks';
 import Task from '../Task/Task';
-import { useEffect, useState } from 'react';
 import { fetchTasks } from '../../Redux/Tasks/tasksRedux';
 // import { useEffect, useState } from 'react';
 
 export default function Tasks() {
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
   // Call the tasks from Redux
-const tasks = useSelector((state) => state.tasks);
-console.log(tasks);
-// console.log(JSON.parse(tasks));
-  
+  const tasks = useSelector((state) => state.tasks);
 
-    useEffect(() => {
-        if (tasks === null) {
-          dispatch(fetchTasks());
-        }
-      }, [dispatch, tasks]);
+  useEffect(() => {
+    if (tasks === null) {
+      dispatch(fetchTasks());
+    }
+  }, [dispatch, tasks]);
 
   return (
     <>
       <AddTasks />
       <Task tasks={tasks} />
-      {/* {
-        (tasks) ?
-        tasks.map((task, index) => <p key={`task${index}`}>{task.taskName}</p>) : <p>No task</p>
-      } */}
     </>
   );
 }
